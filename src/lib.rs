2bd5684b1ab201ec7fa0eb18fc4d1e14ae3211fb
Jamie Blondin <jblondin@gmail.com>
2017-06-02 16:03:54 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+#[macro_export]
+macro_rules! blueprint {
+    ($runner:ident) => {
+        pub mod blueprint_common {
+            use std::env;
+            use std::path::Path;
+            use etl::DataFrame;
+
+            fn usage(exe_name: &str) {
+                println!("Usage: {} <data file> <config file>", exe_name);
+            }
+
+            enum ReturnValue {
+                Success,
+                InvalidArgs,
+                InvalidCall,
+                FileNotFound,
+                EtlError,
+            }
+
+            pub fn main() {
+                let arg_count = env::args().count();
+                if arg_count == 0 {
+                    usage("<executable name>");
+                    ::std::process::exit(ReturnValue::InvalidArgs as i32);
+                } else if arg_count < 3 {
+                    usage(&env::args().next().unwrap()[..]);
+                    ::std::process::exit(ReturnValue::InvalidCall as i32);
+                } else {
+                    let mut args = env::args();
+                    let exe_name = args.next().unwrap();
+                    let data_file = args.next().unwrap();
+                    let config_file = args.next().unwrap();
+
+                    let data_path = Path::new(&data_file[..]);
+                    if !data_path.exists() {
+                        println!("Specified data file {} doest not exist\n", data_file);
+                        usage(&exe_name[..]);
+                        ::std::process::exit(ReturnValue::FileNotFound as i32);
+                    }
+                    let config_path = Path::new(&config_file[..]);
+                    if !config_path.exists() {
+                        println!("Specified config file {} doest not exist\n", config_file);
+                        usage(&exe_name[..]);
+                        ::std::process::exit(ReturnValue::FileNotFound as i32);
+                    }
+
+                    match DataFrame::load(&config_path, &data_path) {
+                        Ok((config, df)) => {
+                            super::$runner(df, config);
+                        }
+                        Err(e) => {
+                            println!("Error loading data file: {}", e);
+                            ::std::process::exit(ReturnValue::EtlError as i32);
+                        }
+                    }
+                    ::std::process::exit(ReturnValue::Success as i32);
+                }
+            }
+        }
+        fn main() {
+            blueprint_common::main();
+        }
+    }
+}
